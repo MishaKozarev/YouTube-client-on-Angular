b4484e1020ca5b02e51e62c98b88ac0f66909236
MishaKozarev <mishaaaaa1986@mail.ru>
2023-10-19 15:23:01 +0300

    feat: generate components and create models

---
+++ youtube-client/src/app/search/search-response.model.ts
@@ -0,0 +1,13 @@
+import {Item} from "./search-item.model";
+
+export interface SearchResponse {
+    TODO: string,
+    kind: string,
+    etag: string,
+    pageInfo: PageInfo,
+    items: Item[]
+  }
+interface PageInfo {
+    totalResults: number,
+    resultsPerPage: number
+}
