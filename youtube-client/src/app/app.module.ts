59fc8c7af87b97b84d528d129f02958a615782b4
MishaKozarev <mishaaaaa1986@mail.ru>
2023-10-23 23:09:18 +0300

    fix: change structure folder

--- youtube-client/src/app/app.module.ts
+++ youtube-client/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HeaderComponent } from './header/header/header.component';
+import { HeaderComponent } from './header/header.component';
 import { SearchItemComponent } from './search/search-item/search-item.component';
 import { SearchResultsComponent } from './search/search-results/search-results.component';
 
