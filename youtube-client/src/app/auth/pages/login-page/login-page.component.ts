62b7ea59adf1e9ea6739ce61d39c5e503acc293f
MishaKozarev <mishaaaaa1986@mail.ru>
2023-11-13 22:44:44 +0300

    feat: add validation rules for login page and apmin page

--- youtube-client/src/app/auth/pages/login-page/login-page.component.ts
+++ youtube-client/src/app/auth/pages/login-page/login-page.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+/* eslint-disable @typescript-eslint/quotes */
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators
+} from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { LoginService } from '../../services/login.service';
@@ -8,24 +16,66 @@ import { LoginService } from '../../services/login.service';
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss']
 })
-export class LoginPageComponent {
-  errorMessage = '';
+export class LoginPageComponent implements OnInit {
+  public loginForm!: FormGroup<{
+    login: FormControl<string | null>;
+    password: FormControl<string | null>;
+  }>;
+
+  public errorMessage = '';
+  public loginErrorMessage = 'Please enter a login email';
 
   constructor(
     private loginService: LoginService,
-    private router: Router
+    private router: Router,
+    private fb: FormBuilder
   ) {}
 
-  public submitForm(login: string, password: string) {
-    if (!login || !password) {
-      this.errorMessage = 'fields must be completed';
+  ngOnInit() {
+    this.loginForm = this.fb.group({
+      login: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  public submitForm() {
+    if (this.loginForm.status === 'INVALID') {
+      if (this.loginForm.value.login === '') {
+        this.loginErrorMessage = '';
+        this.errorMessage = 'Please enter a login email';
+      } else if (this.loginForm.value.password === '') {
+        this.errorMessage = 'Please enter a password';
+      }
     } else {
+      this.loginService.isAuthUser();
       this.loginService.login();
       this.router.navigate(['/youtube']);
     }
   }
 
-  public clearErrorMessage = () => {
+  public clearErrorMessage() {
     this.errorMessage = '';
-  };
+  }
+
+  public checkPasswordValidation() {
+    this.errorMessage = '';
+    if (this.loginForm.value.password!.length < 8) {
+      this.errorMessage =
+        "Your password isn't strong enough: must be at least 8 characters long";
+    } else if (!this.loginForm.value.password!.match(/[A-Z]/)) {
+      this.errorMessage =
+        "Your password isn't strong enough: must contain at least one capital letter";
+    } else if (!this.loginForm.value.password!.match(/[a-z]/)) {
+      this.errorMessage =
+        "Your password isn't strong enough: must contain at least one capital letter";
+    } else if (!this.loginForm.value.password!.match(/\d/)) {
+      this.errorMessage =
+        "Your password isn't strong enough: must contain at least one number";
+    } else if (
+      !this.loginForm.value.password!.match(/[[!@#$&*"'./|/\\+^`~_=]/)
+    ) {
+      this.errorMessage =
+        "Your password isn't strong enough: must contain at least one special character";
+    }
+  }
 }
--- youtube-client/src/app/core/pages/admin-page/admin-page.component.ts
+++ youtube-client/src/app/core/pages/admin-page/admin-page.component.ts
@@ -1,8 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'app-admin-page',
   templateUrl: './admin-page.component.html',
   styleUrls: ['./admin-page.component.scss']
 })
-export class AdminPageComponent {}
+export class AdminPageComponent implements OnInit {
+  public dateErrorMessage = '';
+  public tagErrorMessage = '';
+  public tagsArray: string[] = [];
+
+  public adminForm!: FormGroup<{
+    title: FormControl<string | null>;
+    description: FormControl<string | null>;
+    img: FormControl<string | null>;
+    link: FormControl<string | null>;
+    date: FormControl<string | null>;
+    tag: FormControl<string | null>;
+  }>;
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.adminForm = this.fb.group({
+      title: [
+        '',
+        [Validators.required, Validators.minLength(3), Validators.maxLength(20)]
+      ],
+      description: ['', [Validators.required, Validators.maxLength(255)]],
+      img: ['', [Validators.required]],
+      link: ['', [Validators.required]],
+      date: ['', [Validators.required]],
+      tag: ['', [Validators.required]]
+    });
+  }
+
+  public checkDate() {
+    this.dateErrorMessage = '';
+    const date = this.adminForm.value.date!;
+    const dateArray = date?.split('.');
+    if (date) {
+      if (!/\d./.test(date)) {
+        this.dateErrorMessage = 'Date must not contain letters';
+      } else if (dateArray.length !== 3) {
+        this.dateErrorMessage = 'Enter the date in the specified format';
+      } else if (!(new Date(dateArray.reverse().join('-')) < new Date())) {
+        this.dateErrorMessage = 'The date is invalid';
+      }
+    }
+  }
+
+  public clearErrorMessage() {
+    this.tagErrorMessage = '';
+  }
+
+  public addNewTag() {
+    if (this.adminForm.value.tag) {
+      this.tagErrorMessage = 'You must enter text to add in the field';
+    }
+  }
+}
