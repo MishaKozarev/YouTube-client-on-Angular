fb1ec730c921407144f718692b6111671f2bcc73
MishaKozarev <mishaaaaa1986@mail.ru>
2023-11-29 18:23:08 +0300

    feat: add signup and signin pages on routing

--- connections/connections/src/app/app-routing.module.ts
+++ connections/connections/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignupPageComponent } from './auth/pages/signup-page/signup-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'signup',
+    pathMatch: 'full'
+  },
+  {
+    path: 'signup',
+    component: SignupPageComponent
+  },
+  {
+    path: 'signin',
+    component: SignupPageComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
