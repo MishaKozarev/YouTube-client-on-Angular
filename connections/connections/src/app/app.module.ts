33ec0794f3c2ede95509d00d93695e4064d172a3
MishaKozarev <mishaaaaa1986@mail.ru>
2023-11-29 18:17:20 +0300

    feat: add signin-page component

--- connections/connections/src/app/app.module.ts
+++ connections/connections/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SignupPageComponent } from './auth/pages/signup-page/signup-page.component';
+import { SigninComponent } from './auth/pages/signin/signin.component';
 
 @NgModule({
-  declarations: [AppComponent, SignupPageComponent],
+  declarations: [AppComponent, SignupPageComponent, SigninComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
