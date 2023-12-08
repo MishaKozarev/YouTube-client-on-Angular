788667007ce7f7b120f9ff2583b32c48070c3ada
MishaKozarev <mishaaaaa1986@mail.ru>
2023-12-08 22:22:20 +0300

    feat: change routing on header

--- connections/connections/src/app/core/components/header/header.component.ts
+++ connections/connections/src/app/core/components/header/header.component.ts
@@ -1,8 +1,30 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(private route: Router) {}
+
+  public routingToSignUp() {
+    this.route.navigate(['/signup']);
+  }
+  public routingToSignIn() {
+    this.route.navigate(['/signin']);
+  }
+  public routingToMain() {
+    this.route.navigate(['/main']);
+  }
+  public routingToProfile() {
+    this.route.navigate(['/profile']);
+  }
+  public routingToGroup() {
+    this.route.navigate(['/group']);
+  }
+  public routingToConversation() {
+    this.route.navigate(['/conversation']);
+  }
+}
