3cd840a6d126f6e9f774840c69eebd88a187805a
MishaKozarev <mishaaaaa1986@mail.ru>
2023-12-10 17:46:01 +0300

    feat: move grope effect from connectModule on coreModule

--- connections/connections/src/app/connect/connect.module.ts
+++ connections/connections/src/app/connect/connect.module.ts
@@ -1,10 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreModule } from '@ngrx/store';
-
-import { GroupEffect } from '../store/effects/group.effect';
-import { groupReducer } from '../store/reducers/group.reducer';
 import { GroupListComponent } from './components/group-list/group-list.component';
 import { ConversationPageComponent } from './pages/conversation-page/conversation-page.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
@@ -17,8 +12,6 @@ import { MainPageComponent } from './pages/main-page/main-page.component';
   ],
   imports: [
     CommonModule,
-    StoreModule.forFeature('groupState', groupReducer),
-    EffectsModule.forRoot([GroupEffect])
   ],
   exports: [MainPageComponent, GroupListComponent]
 })
--- connections/connections/src/app/core/core.module.ts
+++ connections/connections/src/app/core/core.module.ts
@@ -5,8 +5,10 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { GroupEffect } from '../store/effects/group.effect';
 
 import { ProfileEffects } from '../store/effects/profile.effect';
+import { groupReducer } from '../store/reducers/group.reducer';
 import { profileReducer } from '../store/reducers/profile.reducer';
 import { HeaderComponent } from './components/header/header.component';
 import { ToastMessageComponent } from './components/toast-message/toast-message.component';
@@ -34,7 +36,8 @@ import { ProfilePageComponent } from './pages/profile-page/profile-page.componen
     ReactiveFormsModule,
     StoreModule.forRoot(),
     StoreModule.forFeature('profileState', profileReducer),
-    EffectsModule.forRoot([ProfileEffects]),
+    StoreModule.forFeature('groupState', groupReducer),
+    EffectsModule.forRoot([ProfileEffects, GroupEffect]),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() })
   ],
   exports: [HeaderComponent, ToastMessageComponent]
