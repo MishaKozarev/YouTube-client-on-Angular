6896ee08149374564625a6558e8828fb8d8dd3b4
MishaKozarev <mishaaaaa1986@mail.ru>
2023-12-17 21:37:38 +0300

    feat: add style people detail page

--- connections/connections/src/app/connect/components/people-details/people-details.component.ts
+++ connections/connections/src/app/connect/components/people-details/people-details.component.ts
@@ -1,7 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators
+} from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 import { getPeopleMessageAction } from 'src/app/store/actions/people-message.actions';
+import { PeopleItem } from 'src/app/store/models/people.models';
+import { selectPeople } from 'src/app/store/selectors/people.selectors';
 import { selectPeopleMessage } from 'src/app/store/selectors/people-message.selectors';
 
 @Component({
@@ -10,19 +19,58 @@ import { selectPeopleMessage } from 'src/app/store/selectors/people-message.sele
   styleUrls: ['./people-details.component.scss']
 })
 export class PeopleDetailsComponent implements OnInit {
+  public peopleMessageForm!: FormGroup<{ peopleMessage: FormControl }>;
   public peopleMessage$ = this.store.select(selectPeopleMessage);
   public currentPeopleId = '';
+  public uid = localStorage.getItem('uid');
+  public timerPeopleSubscription: Observable<number | null> | undefined;
+  public peopleList$!: Observable<PeopleItem[]>;
+  public peopleList!: PeopleItem[];
+  public isShowDeletePeopleDialog: boolean = false;
 
   constructor(
     private store: Store,
-    private routeActive: ActivatedRoute
+    private route: Router,
+    private routeActive: ActivatedRoute,
+    private fb: FormBuilder
   ) {}
-  ngOnInit(): void {
+  public ngOnInit(): void {
     this.routeActive.params.subscribe((params) => {
       this.currentPeopleId = params['conversationID'];
     });
     this.store.dispatch(
       getPeopleMessageAction({ conversationID: this.currentPeopleId })
     );
+
+    this.peopleMessageForm = this.fb.group({
+      peopleMessage: ['', [Validators.required]]
+    });
+
+    this.peopleList$ = this.store.select(selectPeople);
+    this.peopleList$.subscribe((peoples) => {
+      if (peoples) {
+        this.peopleList = peoples;
+      }
+    });
+  }
+
+  public get peopleMessage() {
+    return this.peopleMessageForm.get('peopleMessage') as FormControl;
+  }
+
+  public deletePeopleDialog(): void {
+    this.isShowDeletePeopleDialog = true;
+  }
+  public updateMessage(): void {
+    this.isShowDeletePeopleDialog = false;
+  }
+  public createMessage(): void {
+    this.isShowDeletePeopleDialog = false;
+  }
+  public confirmDeletePeopleDialog(peopleId: string): void {
+    this.route.navigate(['/']);
+  }
+  public noConfirmDeletePeopleDialog(): void {
+    this.isShowDeletePeopleDialog = false;
   }
 }
